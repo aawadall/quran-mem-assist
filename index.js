e1f53ab1ad327ba721f7a01fdb4625b1c1eff98f
EC2 Default User <ec2-user@ip-172-31-42-5.ec2.internal>
2018-04-23 18:11:52 +0000

    0.1.6

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const chalk = require('chalk');
+const express = require('express');
+const api = require("./api/get-data");
+
 const thisPackage = require('./package.json');
 
-const startUp = () =>{
-  console.log(chalk.blue(`Starting ${chalk.bold('QURAN MEMORY ASSISTANT')} version ${thisPackage.version}`));
-};
+const port = 3001;
+
+    // Server 
+    const app = express();
+    
+    // Router 
+    const router = express.Router();
+    
+    // Link Router to methods 
+    router.get('/', (req, res) => {
+        res.json({ message: 'Placeholder'});
+        console.log(chalk.yellow(`Incoming request: ${req.toString()}`));
+        
+    });
+    
+    
+    // Start Server
+    console.log(chalk.blue(`Starting ${chalk.bold('QURAN MEMORY ASSISTANT')} version ${thisPackage.version}`));
+    app.use('/api', router);
+    
+    app.listen(port);
+    console.log(chalk.blue(`Listining @ port ${chalk.bold(port)}`));
 
 
-startUp();
